3dd0878adcab14aae413111b24e4453dbbf025c6
Yavuz Ozmen <yvz.o@Yavuz-MacBook-Pro.local>
2025-05-15 08:55:24 +0200

    Api new

--- src/main/java/com/ozmenyavuz/config/WebConfig.java
+++ src/main/java/com/ozmenyavuz/config/WebConfig.java
@@ -6,10 +6,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/v1/api/**")
-                .allowedOrigins("http://localhost:5173") // React dev sunucusu
+                .allowedOrigins(
+                        "http://localhost:5173", // Lokal geliştirme için
+                        "https://soft-dragon-628aff.netlify.app" // ✅ Netlify frontend adresin
+                )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
